96a076bab3d5edd53598d08654718548ba976869
Trung Tung <tungnguyentrung77@gmail.com>
2022-06-14 18:57:12 +0700

    Ver 1

--- app/src/main/java/com/example/backpackapp/activity/inApp/Overview.kt
+++ app/src/main/java/com/example/backpackapp/activity/inApp/Overview.kt
@@ -15,12 +15,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_overview.*
 
+var layoutContainFragment: ViewPager? = null
+var bottomNavigation: BottomNavigationView? = null
+
 @Suppress("DEPRECATION", "ControlFlowWithEmptyBody", "SameParameterValue")
 class Overview : AppCompatActivity() {
 
     companion object {
-        var layoutContainFragment: ViewPager? = null
-        var bottomNavigation: BottomNavigationView? = null
         fun roundBack() {
             layoutContainFragment?.currentItem = 0
             bottomNavigation?.menu?.findItem(R.id.menu_home_overview)?.isChecked = true
@@ -44,10 +45,10 @@ class Overview : AppCompatActivity() {
         bottomNavigation = findViewById(R.id.bottom_navigation_overview_activity)
         layoutContainFragment = findViewById(R.id.layout_contain_fragment_overview_backpack)
 
-        layout_contain_fragment_overview_backpack.setPageTransformer(true, ZoomOutPageTransformer())
-        layout_contain_fragment_overview_backpack.adapter = viewPagerOverviewAdapter
+        layoutContainFragment?.setPageTransformer(true, ZoomOutPageTransformer())
+        layoutContainFragment?.adapter = viewPagerOverviewAdapter
 
-        layout_contain_fragment_overview_backpack.addOnPageChangeListener(object :
+        layoutContainFragment?.addOnPageChangeListener(object :
             OnPageChangeListener {
             override fun onPageScrolled(
                 position: Int,
@@ -58,13 +59,13 @@ class Overview : AppCompatActivity() {
 
             override fun onPageSelected(position: Int) {
                 when (position) {
-                    0 -> bottom_navigation_overview_activity.menu.findItem(R.id.menu_home_overview).isChecked =
+                    0 -> bottomNavigation?.menu?.findItem(R.id.menu_home_overview)?.isChecked =
                         true
-                    1 -> bottom_navigation_overview_activity.menu.findItem(R.id.menu_chat_overview).isChecked =
+                    1 -> bottomNavigation?.menu?.findItem(R.id.menu_chat_overview)?.isChecked =
                         true
-                    2 -> bottom_navigation_overview_activity.menu.findItem(R.id.menu_location_overview).isChecked =
+                    2 -> bottomNavigation?.menu?.findItem(R.id.menu_location_overview)?.isChecked =
                         true
-                    3 -> bottom_navigation_overview_activity.menu.findItem(R.id.menu_profile_overview).isChecked =
+                    3 -> bottomNavigation?.menu?.findItem(R.id.menu_profile_overview)?.isChecked =
                         true
                 }
             }
@@ -79,13 +80,13 @@ class Overview : AppCompatActivity() {
             BottomNavigationView.OnNavigationItemSelectedListener {
             override fun onNavigationItemSelected(item: MenuItem): Boolean {
                 when (item.itemId) {
-                    R.id.menu_home_overview -> layout_contain_fragment_overview_backpack.currentItem =
+                    R.id.menu_home_overview -> layoutContainFragment?.currentItem =
                         0
-                    R.id.menu_chat_overview -> layout_contain_fragment_overview_backpack.currentItem =
+                    R.id.menu_chat_overview -> layoutContainFragment?.currentItem =
                         1
-                    R.id.menu_location_overview -> layout_contain_fragment_overview_backpack.currentItem =
+                    R.id.menu_location_overview -> layoutContainFragment?.currentItem =
                         2
-                    R.id.menu_profile_overview -> layout_contain_fragment_overview_backpack.currentItem =
+                    R.id.menu_profile_overview -> layoutContainFragment?.currentItem =
                         3
                 }
                 return true
