5cf71c8a5481fa16254d3fa45f87d4f2ef04014d
trungtung03 <68367229+trungtung03@users.noreply.github.com>
2023-07-04 10:25:43 +0700

    Update ZoomOutPageTransformer

--- app/src/main/java/com/example/backpackapp/custom/ZoomOutPageTransformer.kt
+++ app/src/main/java/com/example/backpackapp/custom/ZoomOutPageTransformer.kt
@@ -15,11 +15,9 @@ class ZoomOutPageTransformer : ViewPager.PageTransformer {
             val pageHeight = height
             when {
                 position < -1 -> { // [-Infinity,-1)
-                    // This page is way off-screen to the left.
                     alpha = 0f
                 }
                 position <= 1 -> { // [-1,1]
-                    // Modify the default slide transition to shrink the page as well
                     val scaleFactor = MIN_SCALE.coerceAtLeast(1 - abs(position))
                     val vertMargin = pageHeight * (1 - scaleFactor) / 2
                     val horzMargin = pageWidth * (1 - scaleFactor) / 2
@@ -29,16 +27,13 @@ class ZoomOutPageTransformer : ViewPager.PageTransformer {
                         horzMargin + vertMargin / 2
                     }
 
-                    // Scale the page down (between MIN_SCALE and 1)
                     scaleX = scaleFactor
                     scaleY = scaleFactor
 
-                    // Fade the page relative to its size.
                     alpha = (MIN_ALPHA +
                             (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
                 }
                 else -> { // (1,+Infinity]
-                    // This page is way off-screen to the right.
                     alpha = 0f
                 }
             }
